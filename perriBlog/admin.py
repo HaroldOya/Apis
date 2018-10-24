e4c9321401ecaa88b22470973d805bbd4e042e8d
Harold Oyarzun <har.oyarzun@alumnos.duoc.cl>
2018-10-24 13:58:56 -0300

    Crea aplicacion Django
    crea la aplicacion de django con los requerimientos iniciales

---
+++ perriBlog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Perro
+from .models import Cliente
+
+admin.site.register(Perro)
+admin.site.register(Cliente)
---
+++ perriBlog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Perro(models.Model):
+    Nombre = models.CharField(max_length=200)
+    Raza = models.CharField(max_length=200)
+    Descripcion = models.TextField(max_length=200)
+    Estado = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Nombre
+
+class Cliente(models.Model):
+    Rut = models.CharField(max_length=10)
+    Nombre = models.CharField(max_length=200)
+    FechaNac = models.DateTimeField(blank=True, null=True)
+    Telefono = models.IntegerField
+
+    def __str__(self):
+        return self.Rut
