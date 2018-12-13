30f83e86e945eafdd29f658f27258df8bca8f84e
HaroldOya <44443738+HaroldOya@users.noreply.github.com>
2018-12-13 20:16:42 -0300

    Update views.py

--- perriBlog/views.py
+++ perriBlog/views.py
@@ -1,7 +1,2 @@
 from .models import Perro
-from rest_framework import viewsets
-from perriBlog.serializers import PerroSerializers
 
-class PerroViewSet( viewsets.ModelViewSet ):
-    queryset = Perro.objects.all().order_by ( 'published_date')
-    serializer_class = PerroSerializers
