cef8d0614030202acddb30614e8023b7cedc79a2
HaroldOya <44443738+HaroldOya@users.noreply.github.com>
2018-12-13 20:00:50 -0300

    Update views.py

--- perriBlog/views.py
+++ perriBlog/views.py
@@ -1,6 +1,6 @@
 from .models import Perro
 from rest_framework import viewsets
-from perriblog.serializers import PerroSerializers
+from perriBlog.serializers import PerroSerializers
 
 class PerroViewSet( viewsets.ModelViewSet ):
     queryset = Perro.objects.all().order_by ( 'published_date')
