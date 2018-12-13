f01a2da739cc542ebbd60eedbe19339d77a9d5fc
HaroldOya <44443738+HaroldOya@users.noreply.github.com>
2018-12-13 20:15:39 -0300

    Update urls.py

--- misPerris/urls.py
+++ misPerris/urls.py
@@ -1,15 +1,2 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, include
-from rest_framework import routers
-from perriBlog import views
-from perriBlog.models import Perro
-
-router = routers.DefaultRouter()
-router.register(r'perros', views.PerroViewSet)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url( r'^', include(router.urls)),
-    url( r'^api-auth/', include( 'rest_framework.urls', namespace='rest_framework' ))
-]
